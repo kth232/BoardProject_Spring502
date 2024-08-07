bf725b37720b55a56f45862a44de0570a0ca77d1
kth232 <kth7537@naver.com>
2024-08-07 15:25:33 +0900

    스프링 파일 업로드, 다운로드

--- src/main/resources/static/common/js/FileManager.js
+++ src/main/resources/static/common/js/FileManager.js
@@ -6,37 +6,40 @@ const fileManager = {
     //파일 업로드
     upload(files, gid, location) {
         try {
-            if (!files || files.length == 0){
-                throw new Error("파일을 선택하세요");
+            if (!files || files.length == 0) {
+                throw new Error("파일을 선택 하세요.");
             }
+
             if (!gid || !gid.trim()) {
-                throw new Error("필수 항목 누락입니다(gid).");
+                throw new Error("필수 항목 누락 입니다(gid).");
             }
+
             const formData = new FormData();
             formData.append("gid", gid.trim());
 
             for (const file of files) {
                 formData.append("file", file);
             }
-            if (location && locaiton.trim()) {
+
+            if (location && location.trim()) {
                 formData.append("location", location.trim());
             }
-            const {ajaxLoad}=commonLib; //필요한 기능들을 비구조 할당으로 꺼내서 사용
+            const { ajaxLoad } = commonLib;//필요한 기능들을 비구조 할당으로 꺼내서 사용
 
-            ajaxLoad('file/upload', 'POST', formData)
+            ajaxLoad('/file/upload', 'POST', formData)
                 .then(res => {
-                    if (!res.successs) {
+                    if (!res.success) {
                         alert(res.message);
                         return;
                     }
-                    //파일 업로드 후 처리는 다양함, fileUploadCallback을 직접 상황에 맞게 정의 처린
-                    if(typeof parent.fileUploadCallback === 'function') {
+                    // 파일 업로드 후 처리는 다양, fileUploadCallback을 직접 상황에 맞게 정의 처리
+                    if (typeof parent.fileUploadCallback === 'function') {
                         parent.fileUploadCallback(res.data);
                     }
                 })
                 .catch(err => alert(err.message));
 
-        }catch (e) {
+        } catch (e) {
             console.error(e);
             alert(e.message);
         }
@@ -52,9 +55,9 @@ const fileManager = {
     }
 };
 
-window.addEventListener("DOMContentLoaded", function() {
 
-    //파일 업로드 버튼 이벤트 처리 S
+window.addEventListener("DOMContentLoaded", function() {
+    // 파일 업로드 버튼 이벤트 처리 S
     const fileUploads = document.getElementsByClassName("fileUploads");
     const fileEl = document.createElement("input");
     fileEl.type = 'file';
@@ -62,7 +65,7 @@ window.addEventListener("DOMContentLoaded", function() {
     //파일 데이터만 전송해준다.
 
     for (const el of fileUploads) {
-        el.addEventListener("click", function(){
+        el.addEventListener("click", function() {
             fileEl.value = "";
             delete fileEl.gid;
             delete fileEl.location;
@@ -70,18 +73,19 @@ window.addEventListener("DOMContentLoaded", function() {
 
             //dataset 속성을 통해 데이터 접근
             const dataset = this.dataset;
-            fileEl.gid = dataset.gid; //파일 객체에 키, 값 넣어줌
+            fileEl.gid = dataset.gid;
             if (dataset.location) fileEl.location = dataset.location;
 
             fileEl.click();
+
         });
     }
     //파일 업로드 버튼 이벤트 처리 E
 
     //파일 업로드 처리
-    fileEl.addEventListener("change", function (e) {
+    fileEl.addEventListener("change", function(e) {
        //이벤트 객체에 선택한 파일 정보 담김
-       const files = e.target.files;
+        const files = e.target.files;
         fileManager.upload(files, fileEl.gid, fileEl.location);
         //넣어준 키, 값 업로드
     });
